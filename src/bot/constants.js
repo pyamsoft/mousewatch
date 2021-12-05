014faaf9e5e9d2183f14071024474dea6f756821
pyamsoft <developer.pyamsoft@gmail.com>
2021-12-04 22:25:09 -0800

    Faster intervals

--- src/bot/constants.js
+++ src/bot/constants.js
@@ -6,6 +6,6 @@ const ONE_MINUTE = 60 * ONE_SECOND;
 module.exports = {
   APP_NAME: packageJson.name,
   VERSION: packageJson.version,
-  WATCH_INTERVAL: 3 * ONE_MINUTE,
-  CACHE_INTERVAL: 2 * ONE_MINUTE + 30 * ONE_SECOND,
+  WATCH_INTERVAL: 2 * ONE_MINUTE,
+  CACHE_INTERVAL: 1 * ONE_MINUTE + 30 * ONE_SECOND,
 };
