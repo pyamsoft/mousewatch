83ba8886677ca9983f9d47b062b237013db6f32e
pyamsoft <developer.pyamsoft@gmail.com>
2022-12-07 15:04:02 -0800

    Add help examples

--- src/commands/outputs/help.ts
+++ src/commands/outputs/help.ts
@@ -17,9 +17,35 @@ ${prefix} watch <DATE>      Watch park availability for a DATE
 ${prefix} stop <DATE>       Stop watching a DATE
 ${prefix} cancel            Cancel all pending watch commands
 
+[NOTES]
+DATE must look like MM/DD/YYYY
+
+  01/12/2023
+  12/14/2022
+  12/06/2023
+
 [EXAMPLE]
 
-Examples HERE
+  me >  ${prefix} show 01/24/2023
 
+  bot > Tue, Jan 24, 2023: Inspire Key
+        reservations are AVAILABLE
+        https://disneyland.disney.go.com/entry-reservation/
+
+  me >  ${prefix} watch 01/04/2021
+
+  bot > :thumbsup: Watching Inspire Key
+        reservations on Sun, Dec 18, 2022
+
+  * A few minutes later... *
+
+  bot > @You Sun, Dec 18, 2022: Inspire Key
+        reservations are AVAILABLE
+        https://disneyland.disney.go.com/entry-reservation/
+
+        (React to this message with an emoji to stop
+         watching, otherwise I will assume you did
+         not get a reservation spot, and will keep
+         watching.)
 `);
 };
